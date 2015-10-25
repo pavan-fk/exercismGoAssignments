a6283cce139416d2ff04da17b524f1711d1b87ae
pavank <pavank@flipkart.com>
2015-10-25 23:31:56 +0530

    removed the use of a temp variable + cleaned up go doc

--- leap/leap.go
+++ leap/leap.go
@@ -1,15 +1,9 @@
 package leap
 
-//TestVersion is the exercism test version
+//TestVersion is the exercism test version.
 const TestVersion = 1
 
-//IsLeapYear checks whether a year is a leap year or not
-//it returns a boolean
+//IsLeapYear checks whether a year is a leap year or not.
 func IsLeapYear(year int) bool {
-	var divisor = 4
-	// check for century year
-	if year%100 == 0 {
-		divisor = 400
-	}
-	return (year%divisor == 0)
+	return ((year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
 }
