0f0f66fcde424bf5018296eb0e70a08dc1d625cc
pavank <pavank@flipkart.com>
2015-10-25 23:49:18 +0530

    removed an un-necessary mod check

--- leap/leap.go
+++ leap/leap.go
@@ -5,5 +5,5 @@ const TestVersion = 1
 
 //IsLeapYear checks whether a year is a leap year or not.
 func IsLeapYear(year int) bool {
-	return ((year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
+	return ((year%400 == 0) || (year%100 != 0 && year%4 == 0))
 }
